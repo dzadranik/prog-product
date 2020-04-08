adea2854f8a5bbb1a3dc1c82f6b1a2f322ad5f7e
Victoria <dzadranik@gmail.com>
2020-04-08 23:45:39 +0300

    update template

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-GitHub Pages 
-Inline: https://dzadranik.github.io/i-dex/build/
-With js calendar: https://dzadranik.github.io/i-dex/build/pages/js/index.html
+GitHub Pages
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,4 +3,8 @@ $(function() {
 		visible: 1,
 		itemMargin: 5
 	})
+	$('.jq-menu-button').click(e => {
+		$('.jq-top-panel').toggleClass('show')
+		$(e.currentTarget).toggleClass('open')
+	})
 })
